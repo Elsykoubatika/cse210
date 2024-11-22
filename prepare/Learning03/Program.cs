1647e547c9dd2d003f4dbcf7d8f1d7cd7a9c425e
Elsykoubatika <fkoubatika@gmail.com>
2024-11-22 02:12:20 +0100

    complet

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction(3, 4);
+        fraction.SetBottom(4);
+        fraction.SetTop(3);
+
+        //string fract =   $"{fraction.GetBottom()}/{fraction.GetTop()}";
+
+        //Console.WriteLine(fract);
+
+        Console.WriteLine(fraction.GetBottom());
+        Console.WriteLine(fraction.GetTop());
+
+        Console.WriteLine(fraction.GetDecimelValue());
+        Console.WriteLine(fraction.GetFractionString());
     }
 }
