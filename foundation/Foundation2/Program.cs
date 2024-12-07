9713495099ddc9fdbc6d0a7836f8c060d4028251
Elsykoubatika <fkoubatika@gmail.com>
2024-12-07 14:50:23 +0100

    completed

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address= new Address("34 matsanga St", "Anytown", "york", "USA");
+        Customer customer = new Customer(address, "John Doe");
+        Order order = new Order(customer);
+
+        order.AddProduct(new Product("Laptop", 551, 100, 2));
+        order.AddProduct(new Product("Mouse", 552, 50, 2));
+        order.AddProduct(new Product("Keyboard", 763, 75, 1));
+
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+
+        Console.WriteLine($"Tatol Price = {order.TatolPrice()}");
     }
 }
